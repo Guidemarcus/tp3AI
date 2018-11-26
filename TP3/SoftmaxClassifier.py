81d7efcff7328d18f002a5589e05fae449b0a319
Corentin Bect <corentin.bect@gmail.com>
2018-11-26 10:30:46 -0500

    one_hot

--- TP3/SoftmaxClassifier.py
+++ TP3/SoftmaxClassifier.py
@@ -196,18 +196,24 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
     """
 
     def _one_hot(self, y):
-        maxLength = max(y)
-        resultTab = []
-        for item in y:
-            resultIndex = []
-            for i in range(maxLength):
-                if i == item:
-                    resultIndex.append(1)
-                else:
-                    resultIndex.append(0)
-            resultTab.appen(resultIndex)
+        # Creates a Numpy array
+        np_y = np.array(y)
+        # Training example count
+        m = np_y.size
+        # Different categories
+        categories = np.unique(np_y)
+        # Creates a zero matrix whose number of columns corresponds to the number of categories
+        # and the number of rows corresponds to the numbers of training examples
+        one_hot_array = np.zeros((m, categories.size))
+        # Formats array by replacing categories number by their index in categories array in order
+        # not to exceed one_hot_array size during the next step
+        for index in range(len(categories)):
+            np.place(np_y, np_y == categories[index], index)
 
-        return resultIndex
+        # Sets ones to the concerned values without using loop directly
+        one_hot_array[np.arange(m), np_y] = 1
+
+        return one_hot_array
 
     """
         In :
---
+++ TP3/test.py
@@ -0,0 +1,24 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+import numpy as np
+
+def _one_hot(X):
+    # Creates a Numpy array
+    np_y = np.array(X)
+    # Training example count
+    m = np_y.size
+    # Different categories
+    categories = np.unique(np_y)
+    # Creates a zero matrix whose number of columns corresponds to the number of categories
+    # and the number of rows corresponds to the numbers of training examples
+    one_hot_array = np.zeros((m, categories.size))
+    # Formats array by replacing categories number by their index in categories array in order
+    # not to exceed one_hot_array size during the next step
+    for index in range(len(categories)):
+        np.place(np_y, np_y == categories[index], index)
+
+    # Sets ones to the concerned values without using loop directly
+    one_hot_array[np.arange(m), np_y] = 1
+
+    return one_hot_array
+
+print(_one_hot([1,4,2,5,4]))
