d560f756f6e438f86062dcf2599a8fea641ddddf
Corentin Bect <corentin.bect@gmail.com>
2018-12-05 20:25:37 -0500

    Correctif

--- TP3/test.py
+++ TP3/test.py
@@ -1,26 +1,16 @@
-from sklearn.base import BaseEstimator, ClassifierMixin
-import numpy as np
+from SoftmaxClassifier import SoftmaxClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.model_selection import StratifiedKFold
+    from sklearn.metrics import log_loss
 
+    nb_run = 3
 
-def _one_hot(y):
-    # Creates a Numpy array
-    np_y = np.array(y)
-    # Training example count
-    m = np_y.size
-    # Different categories
-    categories = np.unique(np_y)
-    # Creates a zero matrix whose number of columns corresponds to the number of categories
-    # and the number of rows corresponds to the numbers of training examples
-    yohe = np.zeros((m, categories.size))
-    # Formats array by replacing categories number by their index in categories array in order
-    # not to exceed one_hot_array size during the next step
-    for index in range(len(categories)):
-        np.place(np_y, np_y == categories[index], index)
+    models = [
+        SoftmaxClassifier(),
+        RandomForestClassifier(),
+        DecisionTreeClassifier()
+    ]
 
-    # Sets ones to the concerned values without using loop directly
-    # print(np.arange(m))
-    # print(np_y)
-    yohe[np.arange(m), np_y] = 1
-
-    return yohe
-print(_one_hot([1,2, 5, 4]))
+    scoring = ['neg_log_loss', 'precision_macro','recall_macro','f1_macro']
+    print(compare(models,X_train,y_train_label,nb_run,scoring))
